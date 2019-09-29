43977a7164a1c010afa0bce460e20c00d3681d75
liuyin <liuyin@jingcaiwang.cn>
2019-09-29 10:29:10 +0800

    更新商品排序

--- src/views/GoodsDetail.vue
+++ src/views/GoodsDetail.vue
@@ -11,6 +11,42 @@
       <my-swiper :height="SWIPER_IMAGE_HEIGHT+'px'"
                  :swiperImgs="goodsData.swiperImgs"
                  :paginationType="'2'"></my-swiper>
+
+      <!--内容-->
+      <div class="goods-detail-content-desc">
+        <div class="goods-detail-content-desc-item">
+          <p class="goods-detail-content-desc-item-price">¥{{goodsData.price}}</p>
+          <p class="goods-detail-content-desc-item-name">
+            <direct v-if="goodsData.isDirect"></direct>
+            ¥{{goodsData.name}}</p>
+        </div>
+        <div class="goods-detail-content-desc-item">
+          <p class="goods-detail-content-desc-item-select">已选<span class="single-row-text">{{goodsData.name}}</span></p>
+          <!--附加服务-->
+          <div class="goods-detail-content-desc-item-support">
+              <ul class="goods-detail-content-desc-item-support-list">
+                <li class="goods-detail-content-desc-item-support-list-item"
+                v-for="(item,index) in attachDatas" :key="index">
+                    <img src="@img/support.svg">
+                  <span>{{item}}</span>
+                </li>
+              </ul>
+          </div>
+          <div class="goods-detail-content-desc-detail">
+            <img v-for="(item,index) in goodsData.detailImgs" :key="index" :src="item">
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!--加入购物车 立即购买-->
+    <div class="goods-detail-buy">
+      <div class="goods-detail-buy-add">
+        加入购物车
+      </div>
+      <div class="goods-detail-buy-now">
+        立即购买
+      </div>
     </div>
   </div>
 </template>
@@ -18,19 +54,30 @@
 <script>
 import NavigationBar from '@c/currency/NavigationBar'
 import MySwiper from '@c/swiper/MySwiper'
+import Direct from '@c/goods/Direct'
 export default {
   name: 'GoodsDetail',
   components: {
     NavigationBar,
-    MySwiper
+    MySwiper,
+    Direct
   },
   data () {
     return {
       SWIPER_IMAGE_HEIGHT: 364,
       navBarStyle: {
-        backgroundColor: ''
+        backgroundColor: '',
+        position: 'fixed'
       },
-      goodsData: {}
+      goodsData: {},
+      attachDatas: [
+        '可配送海外',
+        '京东发货&售后',
+        '京准达',
+        '211限时达',
+        '可自提',
+        '不可使用优惠券'
+      ]
     }
   },
   methods: {
@@ -44,6 +91,106 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  @import "@css/style.scss";
+  .goods-detail{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-flow: column;
+    &-nav-left{
+      width: 100%;
+      display: flex;
+      img{
+        align-self: center;
+      }
+    }
+    &-content{
+      overflow: hidden;
+      overflow-y: auto;
+      height: 100%;
+      &-desc{
+        width: 100%;
+        background-color: $bgColor;
+        box-shadow: 0 0 px2rem(16) 0 rgba(0,0,0,0.2);
+        &-item{
+          background-color: white;
+          padding: $marginSize;
+          margin-bottom: $marginSize;
+          &-price{
+            font-size: px2rem(20);
+            color: $mainColor;
+            font-weight: 500;
+          }
+          &-name{
+            margin-top: $marginSize;
+            font-size: $titleSize;
+            font-weight: 500;
+            line-height: px2rem(20);
+          }
+          &-select{
+            font-size: $infoSize;
+            color: $hintColor;
+            height: px2rem(44);
+            display: flex;
+            align-items: center;
+            border-bottom: px2rem(1) solid $lineColor;
+            span{
+              color: $textColor;
+              font-size: $titleSize;
+              font-weight: bold;
+              margin-left: px2rem(4);
+              width: 80%;
+            }
+          }
+          &-support{
+            margin-top: $marginSize;
+            &-list{
+              display: flex;
+              flex-wrap: wrap;
+              &-item{
+                display: flex;
+                align-items: center;
+                padding: px2rem(6) 0;
+                margin-right: $marginSize;
+                img{
+                  width: px2rem(12);
+                  margin-right: px2rem(4);
+                }
+                span{
+                  font-size: $infoSize;
+                  color: $hintColor;
+                };
+              }
+            }
+          }
+        }
+        &-detail{
+          img{
+            width: 100%;
+          }
+        }
+      }
+    }
+    &-buy{
+      background-color: white;
+      border-top: px2rem(1) solid $lineColor;
+      height: 50px;
+      line-height: px2rem(46);
+      text-align: right;
+      div{
+        display: inline-block;
+        width: px2rem(100);
+        text-align: center;
+        font-size: $infoSize;
+        color: white;
+      }
+      &-add{
+        background-color: $mainColor;
+      }
+      &-now{
+        background-color: darkgoldenrod;
+      }
+    }
+  }
 </style>
